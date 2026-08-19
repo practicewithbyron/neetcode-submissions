08fd4f65cb6e1efce6e10d8ab166e34f34f37b48
practicewithbyron <143816275+practicewithbyron@users.noreply.github.com>
2026-08-19 17:10:57 +0100

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.py
@@ -0,0 +1,42 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Binary search
+        # Middle array
+        # Find row by comparing first and last values
+        # Traverse rows
+        # Get middle row
+        # Is target more than the first value and less than last value
+        # - Must be in that row
+        # Is target less than first value,
+        # - Must be in a row before that row
+        # Is target higher than last value,
+        # - Must be in a row later than that row
+        # Find next middle which is the row index of middle of current window
+
+        start_window = 0
+        end_window = len(matrix) - 1
+        break_index = 0
+        while start_window <= end_window:
+            middle = (end_window + start_window) // 2
+            if target >= matrix[middle][0] and target <= matrix[middle][-1]:
+                # In this row, yay
+                for val in matrix[middle]:
+                    if val == target:
+                        return True
+                return False
+            elif target < matrix[middle][0]:
+                # Row before
+                end_window = middle - 1
+            elif target > matrix[middle][-1]:
+                # Row after
+                start_window = middle + 1
+            if break_index == 10:
+                break
+            break_index += 1
+        
+        return False
+            
+
+
+
+        
