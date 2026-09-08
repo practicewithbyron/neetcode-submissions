34cec150aca3141fc5d7e41e576226d7570f9c14
practicewithbyron <143816275+practicewithbyron@users.noreply.github.com>
2026-09-08 09:48:06 +0100

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) - 1);
+        }
+
+        for (int value : count.values()) {
+            if (value != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
