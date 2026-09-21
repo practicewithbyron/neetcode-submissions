af6f90d88254b0c30209e1a5970f4b24d8427a0b
practicewithbyron <143816275+practicewithbyron@users.noreply.github.com>
2026-09-21 09:05:01 +0100

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        if (nums.length <= 1) {
+            return nums;
+        }
+
+        int mid = nums.length / 2;
+
+        int[] left = Arrays.copyOfRange(nums, 0, mid);
+        int[] right = Arrays.copyOfRange(nums, mid, nums.length);
+
+        left = sortArray(left);
+        right = sortArray(right);
+
+        return merge(left, right);
+    }
+
+    private int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        // Compare both arrays while both have elements
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+
+        // Copy remaining elements
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+
+        return result;
+    }
+}
